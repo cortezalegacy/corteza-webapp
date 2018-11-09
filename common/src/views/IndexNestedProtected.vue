45e51a3889becfd605d8629b5dce1ad947ac0656
Tit Petric <black@scene-si.org>
2018-11-09 11:00:16 +0100

    improve auth flow with a reachable mixin

---
+++ common/src/views/IndexNestedProtected.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="centering-wrap inactive-area" v-if="isAuthenticated">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import protect from '@/mixins/protect'
+export default {
+  mixins: [protect]
+}
+</script>
