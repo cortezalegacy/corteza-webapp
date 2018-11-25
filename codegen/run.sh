edf84c7feb213c5c68e981ced6a118e10bc00b44
Tit Petric <black@scene-si.org>
2018-11-25 13:11:33 +0100

    update system apis

--- codegen/run.sh
+++ codegen/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf api && mkdir api
-php codegen.php System ../../crust/system/docs/src/spec.json api/system.js
-php codegen.php CRM ../../crust/crm/docs/src/spec.json api/crm.js
-php codegen.php SAM ../../crust/sam/docs/src/spec.json api/sam.js
+php codegen.php System ../../crust/api/system/spec.json api/system.js
+php codegen.php CRM ../../crust/api/crm/spec.json api/crm.js
+php codegen.php SAM ../../crust/api/sam/spec.json api/sam.js
 cp api/*.js ../common/src/plugins/
