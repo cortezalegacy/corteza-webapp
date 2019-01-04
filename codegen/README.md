a50499301037aace75ea704dcdafe95d27a303ea
Denis Arh <denis.arh@kendu.si>
2019-01-04 11:58:17 +0100

    Codegen removed (replaced by crusttech/crust-api-client.js)

--- codegen/README.md
+++
@@ -1,4 +0,0 @@
-# API client codegen
-
-This is a development-only feature. It generates some code for the clients,
-based on spec files in the main crust repo.
--- codegen/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm -rf api && mkdir api
-php codegen.php System ../../crust/api/system/spec.json api/system.js
-php codegen.php CRM ../../crust/api/crm/spec.json api/crm.js
-php codegen.php SAM ../../crust/api/sam/spec.json api/sam.js
-cp api/*.js ../common/src/plugins/
